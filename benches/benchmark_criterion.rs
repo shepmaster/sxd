55e2f62d0ebb3ae7ab9d5fbf3315cd66a1549a1c
Jake Goulding <jake.goulding@gmail.com>
2021-02-07 21:02:53 -0500

    Add basic Criterion and Iai benchmarks of raw parsing
    Co-authored-by: Dennis Duda <git@seri.tools>

---
+++ benches/benchmark_criterion.rs
@@ -0,0 +1,38 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use sxd_pull_parser::blocking::{Parser, TokenSource};
+
+fn filename() -> &'static str {
+    option_env!("FILE").unwrap_or("examples/standard.xml")
+}
+
+fn parse(file: &str) -> usize {
+    let mut parser = Parser::with_buffer_capacity(file.as_bytes(), 16 * 1024 * 1024);
+
+    let mut count = 0usize;
+    while let Some(v) = parser.next() {
+        v.unwrap();
+        count += 1;
+    }
+
+    count
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("sxd-pull-parser");
+    group.sample_size(20);
+
+    let file = std::fs::read_to_string(filename()).unwrap();
+
+    group.bench_with_input(
+        BenchmarkId::new("raw parse speed", filename()),
+        &file,
+        |b, file| {
+            b.iter(|| parse(file));
+        },
+    );
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ benches/benchmark_iai.rs
@@ -0,0 +1,27 @@
+use sxd_pull_parser::blocking::{Parser, TokenSource};
+
+fn filename() -> &'static str {
+    option_env!("FILE").unwrap_or("examples/standard.xml")
+}
+
+fn parse(file: &str) -> usize {
+    let mut parser = Parser::with_buffer_capacity(file.as_bytes(), 16 * 1024 * 1024);
+
+    let mut count = 0usize;
+    while let Some(v) = parser.next() {
+        v.unwrap();
+        count += 1;
+    }
+
+    count
+}
+
+fn benchmark() {
+    let file = std::fs::read_to_string(filename()).unwrap();
+
+    let count = parse(&file);
+
+    eprintln!("Parsed {} tokens", count);
+}
+
+iai::main!(benchmark);
