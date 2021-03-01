5bf6e48bf9c9674783bf7ca95c5ba74e18f7ac62
Jake Goulding <jake.goulding@gmail.com>
2021-02-28 19:14:23 -0500

    Use the validator in comparison tests

--- comparison/src/lib.rs
+++ comparison/src/lib.rs
@@ -1,12 +1,14 @@
 pub use libxml2_sys;
 pub use sxd_pull_parser;
+pub use sxd_validation;
 
 use sxd_pull_parser::Parser;
+use sxd_validation::Validator;
 
 pub type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 pub fn parse(s: &str) -> Result<usize> {
-    let mut parser = Parser::new(s.as_bytes());
+    let mut parser = Validator::new(Parser::new(s.as_bytes()));
     let mut count = 0;
 
     while let Some(v) = parser.next() {
