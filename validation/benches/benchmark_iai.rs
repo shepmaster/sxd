b57da341a5460000a0dc00397bc3686ef8f51aee
Jake Goulding <jake.goulding@gmail.com>
2021-04-11 14:00:48 -0400

    Add Iai benchmarks to the validator
    This **also** includes the token fuser and the pull-parser
    timing. Once Iai supports it, we should try to separate the fusing and
    parsing into a setup phase.

---
+++ validation/benches/benchmark_iai.rs
@@ -0,0 +1,29 @@
+use pull_parser::Parser;
+use sxd_validation::Validator;
+
+fn filename() -> &'static str {
+    option_env!("FILE").unwrap_or("examples/standard.xml")
+}
+
+fn parse(file: &str) -> usize {
+    let parser = Parser::with_buffer_capacity(file.as_bytes(), 16 * 1024 * 1024);
+    let mut parser = Validator::new(parser);
+
+    let mut count = 0usize;
+    while let Some(v) = parser.next() {
+        v.unwrap();
+        count += 1;
+    }
+
+    count
+}
+
+fn benchmark() {
+    let file = std::fs::read_to_string(filename()).unwrap();
+
+    let count = parse(&file);
+
+    eprintln!("Parsed {} tokens", count);
+}
+
+iai::main!(benchmark);
