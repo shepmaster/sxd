71554f326ddefbcfdb7a23f4e315f26f433d9a2f
Jake Goulding <jake.goulding@gmail.com>
2021-08-11 21:12:48 -0400

    Don't bother with clippy in generated code

--- comparison/libxml2-sys/src/lib.rs
+++ comparison/libxml2-sys/src/lib.rs
@@ -14,6 +14,7 @@ mod ffi {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
     #![allow(dead_code)]
+    #![allow(clippy::all)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
