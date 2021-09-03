9d375aabb0126cf709c3fee10552387bda40d979
Jake Goulding <jake.goulding@gmail.com>
2021-09-02 21:21:12 -0400

    Add an end-to-end test for parsing and formatting a sample of XML
    Before:
    
    ```
      Time (mean ± σ):     930.1 ms ±  26.8 ms    [User: 912.0 ms, System: 18.1 ms]
      Range (min … max):   890.8 ms … 980.0 ms    10 runs
    ```
    
    After:
    
    ```
      Time (mean ± σ):     934.2 ms ±  35.3 ms    [User: 907.2 ms, System: 27.0 ms]
      Range (min … max):   884.9 ms … 990.4 ms    10 runs
    ```
    
    That's 100.44% of the previous time.
    
    ```
      Instructions:          5856389464 (-0.027674%)
      L1 Accesses:           7735028125 (-0.022402%)
      Estimated Cycles:      8092114390 (-0.021363%)
    ```

---
+++ tests/parse-and-format.rs
@@ -0,0 +1,36 @@
+#![deny(rust_2018_idioms)]
+
+use formatter::Formatter;
+use pull_parser::Parser;
+
+type BoxError = Box<dyn std::error::Error>;
+type Result<T = (), E = BoxError> = std::result::Result<T, E>;
+
+#[test]
+fn round_trips_xml() -> Result {
+    let input = r#"
+<?xml version="1.0"?>
+<a name="a">
+  <b1 name="b1" xmlns:ns1="1" xmlns:ns2="2"/>
+  <!--hello-->
+  <b3 name="b3"/>
+  <?hello world?>
+</a>
+"#
+    .trim();
+
+    let mut parser = Parser::new(input.as_bytes());
+    let mut output = Vec::new();
+    let mut fmt = Formatter::new(&mut output);
+
+    while let Some(token) = parser.next_str() {
+        let token = token?;
+        fmt.write_token(token)?;
+    }
+
+    let output = String::from_utf8(output)?;
+
+    assert_eq!(input, output);
+
+    Ok(())
+}
