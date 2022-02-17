03a7c91bb50ddf8df364630abdad5c66a683ffcc
Jake Goulding <jake.goulding@gmail.com>
2022-02-16 20:36:54 -0500

    Expose parsing and fusing from the developer utility
    Before:
    
    ```
      Time (mean ± σ):     925.9 ms ±   4.8 ms    [User: 899.9 ms, System: 26.0 ms]
      Range (min … max):   914.4 ms … 932.2 ms    10 runs
    ```
    
    After:
    
    ```
      Time (mean ± σ):      1.102 s ±  0.019 s    [User: 1.076 s, System: 0.025 s]
      Range (min … max):    1.069 s …  1.119 s    10 runs
    ```
    
    That's 119.02% of the previous time.
    
    ```
      Instructions:          6755760445 (-0.025962%)
      L1 Accesses:           9067860436 (-0.020198%)
      Estimated Cycles:      9424900346 (-0.019521%)
    ```

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 
 use argh::FromArgs;
 use formatter::Formatter;
-use pull_parser::Parser;
+use pull_parser::{Fuse as Fuser, Parser};
 use std::{
     env,
     fs::File,
-    io::{self, BufWriter, Read, Write},
+    io::{self, BufWriter, Write},
     str::FromStr,
 };
 use token::Source;
@@ -18,10 +18,6 @@ type Result<T = (), E = Error> = std::result::Result<T, E>;
 /// The developer utility for SXD
 #[derive(Debug, FromArgs)]
 struct Args {
-    /// do not output the parsed XML
-    #[argh(switch, short = 'q')]
-    quiet: bool,
-
     /// how many bytes to buffer when reading and writing
     #[argh(option)]
     buffer_size: Option<usize>,
@@ -38,6 +34,18 @@ struct Args {
     #[argh(option)]
     output_buffer_size: Option<usize>,
 
+    /// how to process the XML file.
+    ///
+    /// Valid choices: parse, fuse, validate.
+    #[argh(option)]
+    process: Option<ProcessKind>,
+
+    /// how to output the XML file.
+    ///
+    /// Valid choices: count, null, stdout.
+    #[argh(option)]
+    output: Option<OutputKind>,
+
     /// the file to read
     #[argh(positional)]
     filename: String,
@@ -47,19 +55,21 @@ impl Args {
     const DEFAULT_BUFFER_SIZE: usize = 16 * 1024 * 1024;
 
     fn apply_environment_variables(&mut self) {
-        self.quiet = self.quiet || env::var_os("QUIET").is_some();
-
         self.buffer_size.ambient_value("BUFFER_SIZE");
         self.input_buffer_size.ambient_value("INPUT_BUFFER_SIZE");
         self.output_buffer_size.ambient_value("OUTPUT_BUFFER_SIZE");
+
+        self.process.ambient_value("PROCESS");
+        self.output.ambient_value("OUTPUT");
     }
 
     fn into_options(self) -> Options {
         let Self {
-            quiet,
             buffer_size,
             input_buffer_size,
             output_buffer_size,
+            process,
+            output,
             filename,
         } = self;
 
@@ -67,10 +77,14 @@ impl Args {
         let input_buffer_size = input_buffer_size.unwrap_or(buffer_size);
         let output_buffer_size = output_buffer_size.unwrap_or(buffer_size);
 
+        let process = process.unwrap_or_default();
+        let output = output.unwrap_or_default();
+
         Options {
-            quiet,
             input_buffer_size,
             output_buffer_size,
+            process,
+            output,
             filename,
         }
     }
@@ -78,9 +92,10 @@ impl Args {
 
 #[derive(Debug)]
 struct Options {
-    quiet: bool,
     input_buffer_size: usize,
     output_buffer_size: usize,
+    process: ProcessKind,
+    output: OutputKind,
     filename: String,
 }
 
@@ -111,43 +126,163 @@ where
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let Options {
-        quiet,
         input_buffer_size,
         output_buffer_size,
+        process,
+        output,
         filename,
     } = Options::from_env_and_command_line();
 
-    let file = File::open(&filename).expect("Unable to open file");
+    let gen_parser = || {
+        let file = File::open(&filename).expect("Unable to open file");
+        Parser::with_buffer_capacity(file, input_buffer_size)
+    };
 
-    let parser = Parser::with_buffer_capacity(file, input_buffer_size);
+    let gen_fuser = || Fuser::new(gen_parser());
 
-    let count = if quiet {
+    let gen_validator = || Validator::new(gen_parser());
+
+    let gen_quiet = || {
         let out = io::sink();
-        let out = BufWriter::with_capacity(output_buffer_size, out);
-        stream_output(parser, out)
-    } else {
+        BufWriter::with_capacity(output_buffer_size, out)
+    };
+
+    let gen_stdout = || {
         let out = io::stdout();
         let out = out.lock();
-        let out = BufWriter::with_capacity(output_buffer_size, out);
-        stream_output(parser, out)
-    }?;
+        BufWriter::with_capacity(output_buffer_size, out)
+    };
+
+    use {OutputKind as OK, ProcessKind as PK};
+    let mut process: Box<dyn Process> = match (process, output) {
+        (PK::Parse, OK::Count) => CountIndex::new_object(gen_parser()),
+        (PK::Parse, OK::Null) => Format::new_object(gen_parser(), gen_quiet()),
+        (PK::Parse, OK::Stdout) => Format::new_object(gen_parser(), gen_stdout()),
+        (PK::Fuse, OK::Count) => CountIndex::new_object(gen_fuser()),
+        (PK::Fuse, OK::Null) => Format::new_object(gen_fuser(), gen_quiet()),
+        (PK::Fuse, OK::Stdout) => Format::new_object(gen_fuser(), gen_stdout()),
+        (PK::Validate, OK::Count) => CountIndex::new_object(gen_validator()),
+        (PK::Validate, OK::Null) => Format::new_object(gen_validator(), gen_quiet()),
+        (PK::Validate, OK::Stdout) => Format::new_object(gen_validator(), gen_stdout()),
+    };
+
+    let count = process.process()?;
 
     eprintln!("Parsed {} tokens", count);
 
     Ok(())
 }
 
-fn stream_output(parser: Parser<impl Read>, out: impl Write) -> Result<usize> {
-    let mut count = 0;
-    let mut validator = Validator::new(parser);
-    let mut fmt = Formatter::new(out);
+trait Process {
+    fn process(&mut self) -> Result<usize>;
+}
 
-    while let Some(token) = validator.next_str() {
-        let token = token?;
-        count += 1;
+struct CountIndex<I>(I);
 
-        fmt.write_token(token)?;
+impl<'a, I> CountIndex<I>
+where
+    I: Source + 'a,
+{
+    fn new_object(i: I) -> Box<dyn Process + 'a> {
+        Box::new(Self(i))
     }
+}
 
-    Ok(count)
+impl<I: Source> Process for CountIndex<I> {
+    fn process(&mut self) -> Result<usize> {
+        let mut count = 0;
+
+        while let Some(token) = self.0.next_index() {
+            let _token = token?;
+            count += 1;
+        }
+
+        Ok(count)
+    }
+}
+
+struct Format<I, W>(I, W);
+
+impl<'a, I, W> Format<I, W>
+where
+    I: Source + 'a,
+    for<'b> I::StrKind<'b>: formatter::FormatKind,
+    W: Write + 'a,
+{
+    fn new_object(i: I, w: W) -> Box<dyn Process + 'a> {
+        Box::new(Self(i, w))
+    }
+}
+
+impl<I, W> Process for Format<I, W>
+where
+    I: Source,
+    for<'a> I::StrKind<'a>: formatter::FormatKind,
+    W: Write,
+{
+    fn process(&mut self) -> Result<usize> {
+        let mut count = 0;
+        let mut fmt = Formatter::new(&mut self.1);
+
+        while let Some(token) = self.0.next_str() {
+            let token = token?;
+            count += 1;
+
+            fmt.write_token(token)?;
+        }
+
+        Ok(count)
+    }
+}
+
+#[derive(Debug)]
+enum ProcessKind {
+    Parse,
+    Fuse,
+    Validate,
+}
+
+impl Default for ProcessKind {
+    fn default() -> Self {
+        ProcessKind::Validate
+    }
+}
+
+impl FromStr for ProcessKind {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "parse" => Ok(ProcessKind::Parse),
+            "fuse" => Ok(ProcessKind::Fuse),
+            "validate" => Ok(ProcessKind::Validate),
+            _ => Err(format!("unknown process type {s}")),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum OutputKind {
+    Count,
+    Null,
+    Stdout,
+}
+
+impl Default for OutputKind {
+    fn default() -> Self {
+        OutputKind::Stdout
+    }
+}
+
+impl FromStr for OutputKind {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "count" => Ok(OutputKind::Count),
+            "null" => Ok(OutputKind::Null),
+            "stdout" => Ok(OutputKind::Stdout),
+            _ => Err(format!("unknown output type {s}")),
+        }
+    }
 }
