e5656b2fbceb4b3f712f5485cb39d590bafa88cd
Jake Goulding <jake.goulding@gmail.com>
2022-02-23 22:21:07 -0500

    Add CLI options to the developer utility
    These take precedence over the existing environment variables.
    
    Before:
    
    ```
      Time (mean ± σ):     933.7 ms ±   6.8 ms    [User: 908.8 ms, System: 24.9 ms]
      Range (min … max):   921.2 ms … 944.0 ms    10 runs
    ```
    
    After:
    
    ```
      Time (mean ± σ):     925.9 ms ±   4.8 ms    [User: 899.9 ms, System: 26.0 ms]
      Range (min … max):   914.4 ms … 932.2 ms    10 runs
    ```
    
    That's 99.16% of the previous time.
    
    ```
      Instructions:          6757514803 (+0.052689%)
      L1 Accesses:           9069692292 (+0.048912%)
      Estimated Cycles:      9426740537 (+0.047105%)
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![deny(rust_2018_idioms)]
 
+use argh::FromArgs;
 use formatter::Formatter;
 use pull_parser::Parser;
 use std::{
@@ -14,23 +15,108 @@ use validation::Validator;
 type Error = Box<dyn std::error::Error>;
 type Result<T = (), E = Error> = std::result::Result<T, E>;
 
-fn env_or<T>(name: &str, def: T) -> T
+/// The developer utility for SXD
+#[derive(Debug, FromArgs)]
+struct Args {
+    /// do not output the parsed XML
+    #[argh(switch, short = 'q')]
+    quiet: bool,
+
+    /// how many bytes to buffer when reading and writing
+    #[argh(option)]
+    buffer_size: Option<usize>,
+
+    /// how many bytes to buffer when reading.
+    ///
+    /// Defaults to `buffer-size`.
+    #[argh(option)]
+    input_buffer_size: Option<usize>,
+
+    /// how many bytes to buffer when writing.
+    ///
+    /// Defaults to `buffer-size`.
+    #[argh(option)]
+    output_buffer_size: Option<usize>,
+
+    /// the file to read
+    #[argh(positional)]
+    filename: String,
+}
+
+impl Args {
+    const DEFAULT_BUFFER_SIZE: usize = 16 * 1024 * 1024;
+
+    fn apply_environment_variables(&mut self) {
+        self.quiet = self.quiet || env::var_os("QUIET").is_some();
+
+        self.buffer_size.ambient_value("BUFFER_SIZE");
+        self.input_buffer_size.ambient_value("INPUT_BUFFER_SIZE");
+        self.output_buffer_size.ambient_value("OUTPUT_BUFFER_SIZE");
+    }
+
+    fn into_options(self) -> Options {
+        let Self {
+            quiet,
+            buffer_size,
+            input_buffer_size,
+            output_buffer_size,
+            filename,
+        } = self;
+
+        let buffer_size = buffer_size.unwrap_or(Self::DEFAULT_BUFFER_SIZE);
+        let input_buffer_size = input_buffer_size.unwrap_or(buffer_size);
+        let output_buffer_size = output_buffer_size.unwrap_or(buffer_size);
+
+        Options {
+            quiet,
+            input_buffer_size,
+            output_buffer_size,
+            filename,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Options {
+    quiet: bool,
+    input_buffer_size: usize,
+    output_buffer_size: usize,
+    filename: String,
+}
+
+impl Options {
+    fn from_env_and_command_line() -> Self {
+        let mut args: Args = argh::from_env();
+        args.apply_environment_variables();
+        args.into_options()
+    }
+}
+
+trait AmbientValue {
+    fn ambient_value(&mut self, env_var_name: &str);
+}
+
+impl<T> AmbientValue for Option<T>
 where
     T: FromStr,
 {
-    env::var(name)
-        .ok()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(def)
+    fn ambient_value(&mut self, env_var_name: &str) {
+        if self.is_none() {
+            if let Ok(v) = env::var(env_var_name) {
+                *self = v.parse().ok();
+            }
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let quiet = env::var_os("QUIET").is_some();
-    let buffer_size = env_or("BUFFER_SIZE", 16 * 1024 * 1024);
-    let input_buffer_size = env_or("INPUT_BUFFER_SIZE", buffer_size);
-    let output_buffer_size = env_or("OUTPUT_BUFFER_SIZE", buffer_size);
+    let Options {
+        quiet,
+        input_buffer_size,
+        output_buffer_size,
+        filename,
+    } = Options::from_env_and_command_line();
 
-    let filename = env::args().nth(1).expect("Needs one argument");
     let file = File::open(&filename).expect("Unable to open file");
 
     let parser = Parser::with_buffer_capacity(file, input_buffer_size);
